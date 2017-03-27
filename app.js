c20e29885a7a3064935ff3a75bb8b28c52373118
k.liu <k.liu@Ctrip.com>
2017-03-27 16:41:44 +0800

    scope

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if( 'serviceWorker' in navigator){
-			navigator.serviceWorker.register('/service-workers-example/test.js',{scope:'/service-workers-example'}).then(function(reg){
+			navigator.serviceWorker.register('/service-workers-example/test.js',{scope:'/service-workers-example/'}).then(function(reg){
 				//registration worked
 				console.log(reg)
 			}).catch(function(error){
