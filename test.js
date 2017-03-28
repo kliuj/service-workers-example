6c52aec194acea7f5c5f2876a2ae4ada39289cea
lk <991920040@qq.com>
2017-03-28 21:46:22 +0800

    update

--- test.js
+++ test.js
@@ -17,16 +17,17 @@ this.addEventListener('install',function(event){
 })
 this.addEventListener('fetch',function(event){
 	var response;
-	event.respondWith(caches.match(event.request).catch(function(){
-		return fetch(event.request)
-	}).then(function(r){
-		response = r;
-		caches.open('v1').then(function(cache){
-			cache.put(event.request,response)
-		})
-		return response.clone()
-	}).catch(function(){
-		return caches.match('/service-workers-example/images/6.jpg')
-	})
+	event.respondWith(
+		caches.match(event.request).catch(function(){
+			return fetch(event.request).then(function(r){
+				response = r;
+				return caches.open('v1').then(function(cache){
+					cache.put(event.request, response.clone());
+					return response;
+				})
+			})
+		}).catch(function(){
+				return caches.match('/service-workers-example/images/6.jpg')
+			})
 	)
 })
